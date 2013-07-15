8eab0a8a8394b002b6dcb76a2e57ba3882f01f89
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-07-15 06:55:12 -0500

    initial commit

---
+++ lib/Alien/MuPDF.pm
@@ -0,0 +1,23 @@
+package Alien::MuPDF;
+
+use strict;
+use warnings;
+
+use parent 'Alien::Base';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Alien::MuPDF - Alien package for the MuPDF PDF rendering library
+
+=head1 COPYRIGHT
+
+Copyright 2013 Zakariyya Mughal.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the Artistic License version 2.0.
+
+=end
