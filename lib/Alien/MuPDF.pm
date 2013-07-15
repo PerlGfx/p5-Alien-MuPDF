2c2b3ba9ac6d9593024b522b1108aae15918faac
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-07-15 06:55:48 -0500

    Build results of 0ff2b74 (on master)

---
+++ lib/Alien/MuPDF.pm
@@ -0,0 +1,26 @@
+package Alien::MuPDF;
+{
+  $Alien::MuPDF::VERSION = '0.001';
+}
+
+use strict;
+use warnings;
+
+use parent 'Alien::Base';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Alien::MuPDF - Alien package for the MuPDF PDF rendering library
+
+=head1 COPYRIGHT
+
+Copyright 2013 Zakariyya Mughal.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the Artistic License version 2.0.
+
+=end
