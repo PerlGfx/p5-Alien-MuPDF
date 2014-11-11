ac9e624c77e66454d16a654a5383be38a479e234
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-11-11 02:08:51 -0600

    add inline support

--- lib/Alien/MuPDF.pm
+++ lib/Alien/MuPDF.pm
@@ -3,12 +3,28 @@ package Alien::MuPDF;
 use strict;
 use warnings;
 
-use parent 'Alien::Base';
+use parent qw(Alien::Base Exporter);
+our @EXPORT_OK = qw(Inline);
+
+sub Inline {
+	return unless $_[-1] eq 'C'; # Inline's error message is good
+	my $self = __PACKAGE__->new;
+	+{
+		LIBS => $self->libs,
+		INC => $self->cflags,
+		CCFLAGSEX => "-std=c99",
+		AUTO_INCLUDE => '#include "fitz.h"',
+	};
+}
 
 1;
 
 __END__
 # ABSTRACT: Alien package for the MuPDF PDF rendering library
+
+=head1 Inline support
+
+This module supports L<Inline's with functionality|Inline/"Playing 'with' Others">.
 
 =head1 SEE ALSO
 
