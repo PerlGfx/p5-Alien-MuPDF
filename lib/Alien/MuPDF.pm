cc8acb87c896cc1f29e72662904eceec19c38d36
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-11-10 23:32:01 -0600

    update doc POD

--- lib/Alien/MuPDF.pm
+++ lib/Alien/MuPDF.pm
@@ -8,16 +8,8 @@ use parent 'Alien::Base';
 1;
 
 __END__
+# ABSTRACT: Alien package for the MuPDF PDF rendering library
 
-=head1 NAME
+=head1 SEE ALSO
 
-Alien::MuPDF - Alien package for the MuPDF PDF rendering library
-
-=head1 COPYRIGHT
-
-Copyright 2013 Zakariyya Mughal.
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the Artistic License version 2.0.
-
-=end
+L<mupdf|http://mupdf.com/>
