d5362110bcb0323b9c8d70d76ec4c005d557f2c0
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-01-24 14:44:37 -0600

    Release build of v0.002 (on master)

--- lib/Alien/MuPDF.pm
+++ lib/Alien/MuPDF.pm
@@ -1,5 +1,5 @@
 package Alien::MuPDF;
-$Alien::MuPDF::VERSION = '0.001';
+$Alien::MuPDF::VERSION = '0.002';
 use strict;
 use warnings;
 
@@ -36,7 +36,7 @@ Alien::MuPDF - Alien package for the MuPDF PDF rendering library
 
 =head1 VERSION
 
-version 0.001
+version 0.002
 
 =head1 Inline support
 
