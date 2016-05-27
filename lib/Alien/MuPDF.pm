63a879a1f444a7d245ddc5bf9a34a1a0f1fa17cf
Zakariyya Mughal <zaki.mughal@gmail.com>
2016-05-26 23:04:09 -0500

    Modernise the Alien::Base usage

--- lib/Alien/MuPDF.pm
+++ lib/Alien/MuPDF.pm
@@ -3,19 +3,11 @@ package Alien::MuPDF;
 use strict;
 use warnings;
 
-use parent qw(Alien::Base Exporter);
-our @EXPORT_OK = qw(Inline);
+use parent qw(Alien::Base);
 use File::Spec;
 
-sub Inline {
-	return unless $_[-1] eq 'C'; # Inline's error message is good
-	my $self = __PACKAGE__->new;
-	+{
-		LIBS => $self->libs,
-		INC => $self->cflags,
-		CCFLAGSEX => "-std=c99",
-		AUTO_INCLUDE => '#include "mupdf/fitz.h"',
-	};
+sub inline_auto_include {
+	return  [ 'mupdf/fitz.h' ];
 }
 
 sub cflags {
